982ae14085cc31a55da6ee655186d4ba3f002cac
Artem <artsiom.balamutsiuk@instinctools.com>
2019-10-12 16:49:57 +0300

    Created program for colculating resistance

---
+++ 1/2.pas
@@ -0,0 +1,20 @@
+﻿var resistance1: integer;
+var resistance2: integer;
+var resistance3: integer;
+
+var totalConnectionResistance: real;
+
+begin
+  write('Enter resistance R1: ');
+  readln(resistance1);
+  
+  write('Enter resistance R2: ');
+  readln(resistance2);
+  
+  write('Enter resistance R3: ');
+  readln(resistance3);
+  
+  totalConnectionResistance := 1 / ((1 / resistance1) + (1 / resistance2) + (1 / resistance3));
+  
+  write('Total connection resistance: ', totalConnectionResistance);
+end.
