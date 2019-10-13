f71fe88a2850a453eb7ae33bf02feb96ebd32a5b
Artem <artsiom.balamutsiuk@instinctools.com>
2019-10-13 20:16:10 +0300

    Created program that works with modules

---
+++ lab10/angles.pas
@@ -0,0 +1,13 @@
+﻿unit angles;
+
+interface
+function getCtg(angle: real): real;
+
+implementation
+  function getCtg(angle: real): real;
+  begin
+    var ctg: real := cos(angle) / sin(angle);
+  
+    Result := ctg;
+  end;
+end.
---
+++ lab10/task2.pas
@@ -0,0 +1,12 @@
+﻿uses angles;
+
+
+begin
+  write('Enter angle please: ');
+
+  var angle: real;
+  readln(angle);
+  
+  var ctg: real := getCtg(angle);
+  write('Catangens of the angle "', angle, '" equal to ', ctg);
+end.
